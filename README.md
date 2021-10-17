1cf6c1c5ef33b27171d860ca44aa97faca13cc0a
czp3009 <czp3009@gmail.com>
2021-10-17 20:03:49 +0800

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+# Honkit Plugin Encode Link Round With Angle Brackets
+
+Encode url in link that round with angle brackets.
+
+# NPM
+
+```bash
+npm install honkit-plugin-encode-link-round-with-angle-brackets
+```
+
+# Configuration
+
+No config.
+
+# License
+
+Apache 2.0
---
+++ index.js
@@ -0,0 +1,14 @@
+module.exports = {
+    hooks: {
+        "page:before": function (page) {
+            page.content = page.content.replace(/!\[(.*)]\(<(.*)>\)/g, (match, p1, p2) => {
+                //round brackets will not be encoded
+                const encodedP2 = encodeURI(p2).replace(/[!'()*]/g, (char) => {
+                    return "%" + char.charCodeAt(0).toString(16);
+                });
+                return `![${p1}](${encodedP2})`;
+            });
+            return page;
+        }
+    }
+};
