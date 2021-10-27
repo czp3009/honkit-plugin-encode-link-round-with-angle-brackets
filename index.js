fdfc5154574776f21ef896c24f669c8156242fd2
czp3009 <czp3009@gmail.com>
2021-10-27 16:54:57 +0800

    non-greedy

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = {
     hooks: {
         "page:before": function (page) {
-            page.content = page.content.replace(/!\[(.*)]\(<(.*)>\)/g, (match, p1, p2) => {
+            page.content = page.content.replace(/!\[(.*?)]\(<(.*?)>\)/g, (match, p1, p2) => {
                 //round brackets will not be encoded
                 const encodedP2 = encodeURI(p2).replace(/[!'()*]/g, (char) => {
                     return "%" + char.charCodeAt(0).toString(16);
